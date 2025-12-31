d7009c9ea64a4d77f3c55f6e045723d05278bdec
Niko Novak <novakniko2000@gmail.com>
2025-12-31 18:31:32 +0100

    Go Essentials Exercise 3

--- profit_calc/profit_calculator.go
+++ profit_calc/profit_calculator.go
@@ -1,37 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+// Task 3
+// 1) Validate user input --> show error msg & exit if invalid input is provided (no negative numbers, not 0)
+// 2) Store calculated results into file
+
+const resultsFile = "result.txt"
 
 func main() {
-	// var revenue float64
-	// var expenses float64
-	// var taxRate float64
+	revenue, err := GetUserInput("Enter Revenue: ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	expenses, err := GetUserInput("Enter Expenses: ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	taxRate, err := GetUserInput("Enter Tax Rate: ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	// fmt.Print("Enter Revenue: ")  Replace this code with a custom function that outputs the text, waits for user to enter some text, return the entered value
-	// fmt.Scan(&revenue)            In main, call this new made function and store it in one of the variables
-
-	revenue := getUserInput("Enter Revenue: ")
-
-	expenses := getUserInput("Enter Expenses: ")
-
-	taxRate := getUserInput("Enter Tax Rate: ")
-
-	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
+	ebt, profit, ratio := CalculateFinancials(revenue, expenses, taxRate)
 
 	fmt.Printf("EBT: %.1f\n", ebt)
 	fmt.Printf("Profit: %.1f\n", profit)
-	fmt.Printf("Ratio: %.1f\n", ratio)
-
+	fmt.Printf("Ratio: %.3f\n", ratio)
+	storeResults(ebt, profit, ratio)
 }
 
-func getUserInput(outputText string) float64 {
+func storeResults(ebt, profit, ratio float64) {
+	results := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRatio: %.3f\n", ebt, profit, ratio)
+	os.WriteFile(resultsFile, []byte(results), 0644)
+}
+
+func GetUserInput(outputText string) (float64, error) {
 	var userInput float64
 	fmt.Print(outputText)
 	fmt.Scan(&userInput)
-	return userInput
+	if userInput <= 0 {
+		return 0, errors.New("Value must be a positive number.")
+	}
+	return userInput, nil
 }
 
-func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
+func CalculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
 	ebt := revenue - expenses
 	profit := ebt * (1 - taxRate/100)
 	ratio := ebt / profit
