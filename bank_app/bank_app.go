3eeb7ea85c8475d7500e4d026a56add97bf2a8fa
Niko Novak <novakniko2000@gmail.com>
2025-12-31 17:54:02 +0100

    Go Essentials Exercise, rw files

--- bank_app/bank_app.go
+++ bank_app/bank_app.go
@@ -1,25 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const accountBalanceFile = "balance.txt"
+
+func GetBalanceFromFile() (float64, error) {
+	data, err := os.ReadFile(accountBalanceFile)
+
+	if err != nil {
+		return 1000, errors.New("Failed to find balance file.")
+	}
+
+	balanceText := string(data)
+	balance, err := strconv.ParseFloat(balanceText, 64)
+
+	if err != nil {
+		return 1000, errors.New("Failed to parse stored balance value.")
+	}
+
+	return balance, nil
+}
+
+func WriteBalanceToFile(balance float64) {
+	balanceText := fmt.Sprint(balance)
+	os.WriteFile(accountBalanceFile, []byte(balanceText), 0644) // 0644, way of encoding file permissions --> this set means the owner of the file will be able to read and write the file, while others just read
+}
+
+func CheckAccountBalance(balance float64) {
+	fmt.Println("Your balance is: ", balance)
+}
+
+func BankOptions() {
+	fmt.Println("What do you want to do?")
+	fmt.Println("1. Check the balance")
+	fmt.Println("2. Deposit money")
+	fmt.Println("3. Withdraw money")
+	fmt.Println("4. Exit")
+}
 
 func main() {
-	var accountBalance float64 = 1000
+	var accountBalance, err = GetBalanceFromFile()
+
+	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println(err)
+		fmt.Println("--------------")
+		// panic(err)
+	}
 
 	fmt.Println("Welcome to the Bank App!")
-
 	for {
-		bankOptions()
+		BankOptions()
 		var userChoice int
 		fmt.Print("Please input the number of the choosen action(1-4): ")
 		fmt.Scan(&userChoice)
 		fmt.Println("Your choice: ", userChoice)
 
-		// Can store checks in variables and get a value of type boolean
-		// wantsCheckBalance := userChoice == 1
-
-		if userChoice == 1 {
-			checkAccountBalance(accountBalance)
-		} else if userChoice == 2 {
+		switch userChoice {
+		case 1:
+			CheckAccountBalance(accountBalance)
+		case 2:
 			fmt.Print("Your deposit amount: ")
 			var depositAmount float64
 			fmt.Scan(&depositAmount)
@@ -29,9 +74,11 @@ func main() {
 				continue
 			}
 			accountBalance += depositAmount
-			checkAccountBalance(accountBalance)
-		} else if userChoice == 3 {
-			checkAccountBalance(accountBalance)
+			CheckAccountBalance(accountBalance)
+			fmt.Print("Writing to file!")
+			WriteBalanceToFile(accountBalance)
+		case 3:
+			CheckAccountBalance(accountBalance)
 			fmt.Print("Amount of money you wish to withdraw: ")
 			var withdrawalAmount float64
 			fmt.Scan(&withdrawalAmount)
@@ -49,27 +96,13 @@ func main() {
 			} else {
 				fmt.Println("You successfully withdraw this amount: ", withdrawalAmount)
 				accountBalance -= withdrawalAmount
-				checkAccountBalance(accountBalance)
+				CheckAccountBalance(accountBalance)
+				WriteBalanceToFile(accountBalance)
 			}
-		} else if userChoice == 4 {
+		default:
 			fmt.Println("Exited. Goodbye!")
-			break
-		} else {
-			fmt.Println("Error, did not recognise number!")
+			fmt.Println("Thanks for choosing our bank!")
+			return
 		}
 	}
-
-	fmt.Println("Thanks for choosing our bank!")
-}
-
-func checkAccountBalance(balance float64) {
-	fmt.Println("Your balance is: ", balance)
-}
-
-func bankOptions() {
-	fmt.Println("What do you want to do?")
-	fmt.Println("1. Check the balance")
-	fmt.Println("2. Deposit money")
-	fmt.Println("3. Withdraw money")
-	fmt.Println("4. Exit")
 }
